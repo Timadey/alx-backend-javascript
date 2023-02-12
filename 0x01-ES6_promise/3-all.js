f9036cdd4ed7be1b57d7ab966cc8e78f4f0cf4fc
Timadey <dlktimothy@gmail.com>
2023-02-12 08:21:44 +0100

    Fix error

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,5 +4,5 @@ export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()]).then((val) => {
     const res = Object.assign({}, ...val);
     console.log(`${res.body} ${res.firstName} ${res.lastName}`);
-  }).catch(() => console.log('Signup system offline'));
+  }).catch(() => { console.log('Signup system offline'); });
 }
