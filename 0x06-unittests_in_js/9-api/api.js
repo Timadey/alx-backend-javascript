e0894251f8cf3158bfba502d5d9a620533ff0c78
Timadey <dlktimothy@gmail.com>
2023-04-20 03:45:54 +0100

    add unittest in js

---
+++ 0x06-unittests_in_js/9-api/api.js
@@ -0,0 +1,17 @@
+// import express from 'express';
+const express = require('express');
+
+const port = 7865;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id([0-9]+)', (req, res) => {
+  res.send(`Payment methods for cart ${req.params.id}`);
+});
+
+app.listen(port, () => {
+  console.log(`API available on localhost port ${port}`);
+});
